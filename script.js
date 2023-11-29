ab5d3e473951be7a3009e95ba6acca0cd73ef6f9
Keshav Narang <mrkeshavnarang@gmail.com>
2023-11-28 17:42:03 -0800

    automate system

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ fetch('data.json')
   .then(response => response.json())
   .then(data => {
     // Get the current date in the required format, e.g., 'October_29_2023'
-    const currentDate = 'October_29_2023'; // Replace this with your logic to get the current date
+    const currentDate = 'November_28_2023'; // Replace this with your logic to get the current date
     
     const content = data[currentDate];
 
     // Update elements in your HTML based on the fetched data
-    document.querySelector('#SP').setAttribute('src', content.sp_image);
-    document.querySelector('#Dow_Jones').setAttribute('src', content.dow_jones_image);
+    document.querySelector('#Top_Gainers').setAttribute('src', content.top_gainers_image);
+    document.querySelector('#Top_Losers').setAttribute('src', content.top_losers_image);
+	
+	const activelyTradedTable = document.getElementById('Most_Actively_Traded_Body');
+
+    content.most_actively_traded.Company.forEach((company, index) => {
+      const row = document.createElement('tr');
+      const companyCell = document.createElement('td');
+      const changePercentageCell = document.createElement('td');
+
+      companyCell.textContent = company;
+      changePercentageCell.textContent = content.most_actively_traded['Change Percentage'][index];
+
+      row.appendChild(companyCell);
+      row.appendChild(changePercentageCell);
+
+      activelyTradedTable.appendChild(row);
+	});
 
     document.querySelector('#sports_equity_paragraph').textContent = content.sports_equity_paragraph;
     document.querySelector('#real_estate_paragraph').textContent = content.real_estate_paragraph;
