80983f59c9250c8907ad8d0e3d735b839b4553b1
Keshav Narang <mrkeshavnarang@gmail.com>
2023-12-05 16:58:09 -0800

    refining absolute paths

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Fetch data from JSON file
-fetch('/data.json')
+fetch('data.json')
   .then(response => response.json())
   .then(data => {
     // Get the current date in the required format, e.g., 'October_29_2023'
